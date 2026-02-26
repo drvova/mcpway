ffc65f3f7bbecca0228b44cf4f6b5eed7d6d595a
drvova <drvova@openwarp.local>
2026-02-25 23:48:45 -0600

    Vendor protoc for proto codegen in CI

--- rust/build.rs
+++ rust/build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
+    std::env::set_var("PROTOC", protoc_path);
     tonic_prost_build::configure().compile_protos(&["proto/mcpway_bridge.proto"], &["proto"])?;
     println!("cargo:rerun-if-changed=proto/mcpway_bridge.proto");
     Ok(())
